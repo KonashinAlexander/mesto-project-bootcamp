0a8762f69183b014f7de5a0ecac5fe7ae34494ee
Alexander Konashin <konashin.alexander@yandex.ru>
2023-01-13 09:44:28 +0300

    feat(script.js): add open-close buttons

---
+++ script.js
@@ -0,0 +1,14 @@
+let popup = document.querySelector('.popup');
+let editButton = document.querySelector('.profile__edit-button');
+let closeButton = document.querySelector('.form__close-button');
+
+function popupOpen(arg) {
+    popup.classList.add('popup_opened');
+}
+
+function popupClose(arg2) {
+    popup.classList.remove('popup_opened');
+}
+
+editButton.addEventListener('click', popupOpen);
+closeButton.addEventListener('click', popupClose);
