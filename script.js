f3e7e25d54b22a029f870a5da72756f69ec92dc3
Alexander Konashin <konashin.alexander@yandex.ru>
2023-01-14 12:54:50 +0300

    fix(form, element, profile): change to flex

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let popup = document.querySelector('.popup');
 let editButton = document.querySelector('.profile__edit-button');
-let closeButton = document.querySelector('.form__close-button');
+let closeButton = document.querySelector('.popup__close-button');
 
 function popupOpen(arg) {
     popup.classList.add('popup_opened');
