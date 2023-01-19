c9b7481fa10110aa94b79d2b68eb94a2bc8ff1ba
Alexander Konashin <konashin.alexander@yandex.ru>
2023-01-19 09:16:53 +0300

    feat(cards_grid): add cards from JS to page
    create card template, clone card, add to cards grid

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
+// 1 добавляем попап на страницу и активизируем кнопки //
+
 const popup = document.querySelector('.popup');
 const editButton = document.querySelector('.profile__edit-button');
 const closeButton = document.querySelector('.popup__close-button');
 
 
 function popupOpen(arg) {
-    // popup.classList.add('popup_opened');
     popup.style.visibility='visible';
 }
 
 function popupClose(arg2) {
-    // popup.classList.remove('popup_opened');
     popup.style.visibility='hidden';
 }
 
@@ -37,4 +37,44 @@ formElement.addEventListener('submit', function(evt) {
     popupClose();
 });
 
+// 2 JS добавляет 6 карточек при загрузке //
+
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
+initialCards.forEach(element => {
+    const cardTemplate = document.querySelector('#card').content;
+    const cardGrid = document.querySelector('.elements');
+
+    const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
+
+    cardElement.querySelector('.element__picture').src = element.link;
+    cardElement.querySelector('.element__text').textContent = element.name;
+
+    cardGrid.prepend(cardElement);
+});
 
