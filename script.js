b31ea7bb51c26ec33b013854cc3a908ed1a3a0dc
Alexander Konashin <konashin.alexander@yandex.ru>
2023-01-22 08:02:12 +0300

    feat(popup): create smooth open

--- script.js
+++ script.js
@@ -47,18 +47,32 @@ function loadCards() {
 
 loadCards();
 
+// объявляем функцию открытия попап + form
+
+function formOpen() {
+  popup.classList.add('popup_click_button');
+  
+  const formTemplate = document.querySelector('#form').content;
+  const formElement = formTemplate.querySelector('.popup__container').cloneNode(true);
+  return formElement;
+}
+
+// объявляем функцию закрытия попап
+
+function popupClose() {
+  popup.classList.remove('popup_click_picture');
+  popup.classList.remove('popup_click_button');
+  document.querySelector('.popup__container').remove();
+}
+
 // 2. клонируем форму, добавляем функцию редактирования профиля //
 
 const popup = document.querySelector('.popup');
 const editButton = document.querySelector('.profile__edit-button');
 
 function editProfile() {
- 
-  popup.style.visibility='visible';
-
-  const formTemplate = document.querySelector('#form').content;
-  const formElement = formTemplate.querySelector('.popup__container').cloneNode(true);
-
+  
+  const formElement = formOpen();
   formElement.querySelector('.form__heading').textContent = "Редактировать профиль";
   formElement.querySelector('#line-1').value = 'Жак-Ив Кусто';
   formElement.querySelector('#line-2').value = 'Исследователь океана';
@@ -74,15 +88,9 @@ function editProfile() {
     proileTitle.textContent = formElement.querySelector('#line-1').value;
     profileSubtitle.textContent = formElement.querySelector('#line-2').value;
 
-    popup.style.visibility='hidden';
-  
+    popupClose();
     
   });
-
-  function popupClose() {
-    popup.style.visibility='hidden';
-        
-  }      
 
   formElement.querySelector('.popup__close-button').addEventListener('click', popupClose);
 };
@@ -94,41 +102,30 @@ editButton.addEventListener('click', editProfile);
 const addButton = document.querySelector('.profile__add-button');
 
 function editPictures() {
-  popup.style.visibility='visible';
-  
-  const formTemplate = document.querySelector('#form').content;
-  const formElement = formTemplate.querySelector('.popup__container').cloneNode(true);
 
-  popup.append(formElement);
-
+  const formElement = formOpen();
   formElement.querySelector('.form__heading').textContent = "Новое место";
   formElement.querySelector('#line-1').value = 'Название';
   formElement.querySelector('#line-2').value = 'Ссылка на картинку';
   
-  
+  popup.append(formElement);
 
-    formElement.addEventListener('submit', function(evt) {
-      evt.preventDefault();
+  formElement.addEventListener('submit', function(evt) {
+    evt.preventDefault();
 
-      const cardTemplate = document.querySelector('#card').content;
-      const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
+    const cardTemplate = document.querySelector('#card').content;
+    const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
 
-      cardElement.querySelector('.element__picture').src = formElement.querySelector('#line-2').value;
-      cardElement.querySelector('.element__text').textContent = formElement.querySelector('#line-1').value;
+    cardElement.querySelector('.element__picture').src = formElement.querySelector('#line-2').value;
+    cardElement.querySelector('.element__text').textContent = formElement.querySelector('#line-1').value;
 
-      cardGrid.prepend(cardElement);
-            
-      popup.style.visibility='hidden';
-      document.querySelector('.popup__container').remove();
-      
-    });
+    cardGrid.prepend(cardElement);
+          
+    popupClose();
+    
+  });
 
-  function popupClose() {
-        popup.style.visibility='hidden';
-        document.querySelector('.popup__container').remove();
-    }
-
-  formElement.querySelector('.popup__close-button').addEventListener('click', popupClose);
+    formElement.querySelector('.popup__close-button').addEventListener('click', popupClose);
 };
 
 addButton.addEventListener('click', editPictures);
@@ -136,28 +133,21 @@ addButton.addEventListener('click', editPictures);
 // 4.  добавить кнопку удаления крточки и функцию //
 
 function deleteCard(but) {
-  
   but.closest('.element').remove();
 };
 
 
 // 5. меняем цвет сердечка //
 
-
-
 function changeColor(evt) {
- 
   evt.classList.toggle('element__like-button_color_black');
 }
-
-
 
 // 6. открываем попап и увеличиваем картинку //
 
 function enchancePicture(card) {
-  popup.style.visibility = 'visible';
-  popup.style.backgroundColor = 'rgba(0 0 0 / 0.9)';
-
+  popup.classList.add('popup_click_picture');
+  
   const pictureTemplate = document.querySelector('#picture').content;
   const pictureElement = pictureTemplate.querySelector('.popup__container').cloneNode(true);
 
@@ -165,11 +155,6 @@ function enchancePicture(card) {
   pictureElement.querySelector('#text').textContent = card.parentNode.querySelector('.element__text').textContent;
 
   popup.append(pictureElement);
-
-  function popupClose() {
-    popup.style.visibility='hidden';
-    document.querySelector('.popup__container').remove();
-  }
 
   pictureElement.querySelector('.popup__close-button').addEventListener('click', popupClose);
 
