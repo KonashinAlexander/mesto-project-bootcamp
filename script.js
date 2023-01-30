3c722b39f4ea37d7b231b8d85beb2699de25ed47
Alexander Konashin <konashin.alexander@yandex.ru>
2023-01-30 12:30:33 +0300

    feat(form, popup): add forms validation, popup close via Esc and click

--- script.js
+++ script.js
@@ -70,9 +70,7 @@ function loadCards() {
 
 loadCards();
 
-// document.addEventListener('click', e => console.log(e.target));
-// document.addEventListener('click', function(){ console.log(this); arguments});
- 
+
 // 2. функции открытия и закрытия попап
 
 const popupProfile = document.querySelector('#popup_profile');
@@ -109,7 +107,7 @@ const bigPictureBtn = popupPicture.querySelector('.popup__close-button');
 
 function enchancePicture(image) {
   popupPicture.classList.add('popup_opened');
-
+  popupPicture.style.backgroundColor = 'rgba(0 0 0 / 0.9)';
   bigPicture.src = image.src;
   bigPictureText.textContent = image.alt;
   bigPictureBtn.addEventListener('click', event => {
@@ -175,12 +173,115 @@ function changeColor(evt) {
 };
 
 
+// VALIDATION
+
+
+// Функция, которая добавляет класс с ошибкой
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add('form__item_type_error');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('form__item-error_active');
+};
+
+// Функция, которая удаляет класс с ошибкой
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove('form__item_type_error');
+  errorElement.classList.remove('form__item-error_active');
+  errorElement.textContent = '';
+};
+
+// Функция, которая проверяет валидность поля
+const isValid = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+// функция которая устанавливает слушатель для всех инпутов одной формы
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll('.form__item'));
+  const buttonElement = formElement.querySelector('.form__button');
+
+  toggleButtonState(inputList, buttonElement);
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', function () {
+      isValid(formElement, inputElement);
+
+      toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
+
+// функция которая подключает валидацию ко всем формам
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll('.form'));
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', function (evt) {
+      evt.preventDefault();
+    });
+
+    setEventListeners(formElement);
+  });  
+};
 
 
 
 
 
+// переключение кнопки с активной на неактивную
+
+const hasInvalidInput = (inputList) => {
+  
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  })
+};
 
 
+const toggleButtonState = (inputList, buttonElement) => {
+  
+  if (hasInvalidInput(inputList)) {
+    
+    buttonElement.classList.add('form__button_inactive');
+  } else {
+    
+    buttonElement.classList.remove('form__button_inactive');
+    buttonElement.removeAttribute('disabled');
+  }
+};
 
 
+enableValidation(); // вызвали функцию валидации всех полей всех форм
+
+
+// document.addEventListener('click', e => console.log(e.target));
+// document.addEventListener('click', function(){ console.log(this); arguments});
+
+// document.querySelector('.popup_opened').addEventListener('click', e => console.log(e.target));
+
+
+// функция которая закрывает попап кликом мышки
+function checkPopupOpened (agr1) {
+  if (agr1.classList.contains('popup_opened')) {
+    agr1.classList.remove('popup_opened');
+  }
+};
+
+document.addEventListener('click', e => checkPopupOpened(e.target));
+
+
+// функция которая закрывает попап нажатием Esc
+function checkEscape (arg2, popup) {
+  if (arg2 === 'Escape') {
+    closePopup(popup)
+  }
+}
+
+document.addEventListener('keydown', e => checkEscape(e.key, popupProfile));
+document.addEventListener('keydown', e => checkEscape(e.key, popupCard));
+document.addEventListener('keydown', e => checkEscape(e.key, popupPicture));
